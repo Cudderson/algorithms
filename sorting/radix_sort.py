0df07052aace70430a94a55f6316e8ae359f966d
cudderson <codered1227@gmail.com>
2021-03-30 14:24:26 -0500

    radix sort complete, need to tighten explanation

--- sorting/radix_sort.py
+++ sorting/radix_sort.py
@@ -27,7 +27,7 @@ def counting_sort(array, place):
     while i >= 0:
         index = array[i] / place
 
-        # **********
+        # ********** notice how similar this is to line 19
         output[count[int(index % 10)] - 1] = array[i]
 
         # By decrementing count[], we can safely place the next value in output while maintaining stability (flattening)
@@ -53,7 +53,7 @@ def radix_sort(array):
         place *= 10
 
 
-numbers = [88, 17, 64, 2, 12, 0, 141, 29, 18, 77]
+numbers = [304, 425, 406, 323, 289, 385]
 
 print(f"Unsorted Array: {numbers}")
 radix_sort(numbers)
@@ -96,26 +96,57 @@ print(f"Sorted array: {numbers}")
 # 3. Next, we enter a for loop (for i in range(1, 10)) that performs one operation: 'count[i] += count[i - 1]'
 #   - The purpose of this loop is to calculate the index where each element belongs.
 #   - (ex. [1, 2, 4, 4, 5, 5, 5, 7, 9, 10])
-#   - *** come back to this ***
+#   - Notice that the last value in 'count' is equal to the length of the array.
+# I think we do the 'rolling sum' in count to give an actual position to each of the 'bucket' values. By adding all of the
+# buckets together, we are creating index positions for the output array, as the final bucket will always refer to the
+# position in output array with the highest value (for place being evaluated)
 
 # 4. Next, we build our 'output' array, by iterating over our original array in reverse order.
 #   - 'i' is set to 'n' - 1 (0-based indexing)
 #   - We enter a while loop (while i >= 0) to traverse our original array backwards
 #   - 'index' is set to 'array[i] / place'
-#   - We then put the value at 'array[i]' into 'output[]', at the position of 'count[int(index % 10)] - 1'
-#       - Assume our 'index' is 77.0
-#       - 'count[int(index % 10)] - 1' == 'count[int(7)] - 1' == 'subtract 1 from value at count[7]'
-#       - We place 'array[i]' at that position in 'output[]'
-#       - 'i' is decremented
-# ***** begin here *****
+#   - We then put the value 'array[i]' into 'output[]', at the position of 'count[int(index % 10)] - 1'
+#       -  breakdown: 'output[count[int(index % 10)] - 1] = array[i]'
+#           - index = 385.0
+#           - int(index % 10) = 5
+#           - output[count[5] - 1] = array[i], and (count[5] == 4)
+#           - output[4 - 1] or output[3] = array[i]
+#   - Then, 'count[int(index % 10)] -= 1' executes, which will decrement the value at 'count[]' that we just evaluated in 'output[count[int(index % 10)] - 1] = array[i]'
+#   - 'i' is decremented to continue looping
 
-# i = n - 1
-# while i >= 0:
-#     index = array[i] / place
-#
-#     output[count[int(index % 10)] - 1] = array[i]
-#
-#     # By decrementing count[], we can safely place the next value in output while maintaining stability (flattening)
-#     count[int(index % 10)] -= 1
-#
-#     i -= 1
+# * iteration 2:
+#   - index = 289.0
+#   - int(index % 10) = 9
+#   - output[count[9] - 1] = array[i], and (count[9] == 6)
+#   - output[6 - 1] or output[5] = array[i]
+# Then, 'count[int(index % 10)] -= 1' executes, which will decrement the value at 'count[]' that we just evaluated in 'output[count[int(index % 10)] - 1] = array[i]'
+#   - This way, if the next value examined refers to the same position in 'count[]', it will be place in its own position in 'output[]'
+# 'i' decrements
+
+# We use '-1' to preserve 0-based indexing. Because the total at the end of 'count[]' records the total number of values 'n',
+# and the max-position in output is 'n - 1'
+# We are also stably-sorting these values. As we iterate backwards in this loop, we are ensuring that a value appearing at
+# the end of our original array will be placed higher in the output array than another element of the same value.
+
+# After all iterations, our output array is sorted by the current 'place' we're examining: [323, 304, 425, 385, 406, 289]
+
+# From here, the output array is copied to the original array.
+
+# We return to radix_sort(), where 'place' is multiplied by 10, place = 10 now. This is to examine the 'tens' place of
+# the values in our array for sorting.
+
+# The same process repeats, of defining an index (array[i] / place), and incrementing a bucket in 'count' with (index % 10).
+# Then, the 'rolling sum' step executes, giving context to each bucket as to where the value relating to that bucket should
+# be placed in output[].
+# Then, we build the output array.
+# We iterate over our array backwards, defining an index the same way (array[i] / place), and using that index's 'bucket' value, 'count[int(index % 10)]',
+# to determine the position for array[i] in output[]. (Remember, we do '-1' to preserve 0-based indexing)
+# We also decrement the bucket value: 'count[int(index % 10)] -= 1' so that another value with the same bucket isn't placed
+# at the same spot in the output array. Also, because we're iterating backwards, values with the same bucket will be placed
+# appropriately in the output array. (ex. Iterating forwards would place a value in output[], decrement the bucket, and the
+# next value relating to that bucket would be placed *before* the first value, therefore disrupting the original order. By
+# iterating backwards, we ensure that values maintain stability/order as they appear in the array.)
+# Eventually, the base-case will trigger, when maximum / place is not greater than 0. At this point, we know that we have
+# examined the most-significant-digit of the largest value in the original array; the array is sorted.
+
+# **clean up explanation**
